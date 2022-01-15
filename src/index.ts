185a7d1c94b784f9957fb76a922c2f0f539fb4e6
David Letaw <jdletaw@gmail.com>
2022-01-15 15:34:40 -0800

    creating first pass at first post

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello there");
+import { MikroORM } from "@mikro-orm/core";
+import {_prod_} from "./constants";
+import {Post} from "./entities/Post";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: "dathread",
+        type: "postgresql",
+        debug: !_prod_,
+    });
+
+    const post = orm.em.create(Post, {title: "my first post"})
+    await orm.em.persistAndFlush(post);
+}
+
+main().catch((err) => {
+    console.error(err);
+});
+
